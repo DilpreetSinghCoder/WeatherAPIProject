508f08fc4dcd8b16869af99b7717e7bd6bd8dcc5
smitshah1711 <61113890+smitshah1711@users.noreply.github.com>
2024-03-27 09:48:30 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
+func main() {
+	http.HandleFunc("/city", handleCityRequest)
+	fmt.Printf("Server is starting on port: %v\n", Dport)
+	http.ListenAndServe(Dport, nil)
+}
+
+func postWeather(w http.ResponseWriter, r *http.Request) {
+	var requestData struct {
+		Name string `json:"name"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, "Error decoding request body", http.StatusBadRequest)
+		return
+	}
+
+	cityName := requestData.Name
+	if cityName == "" {
+		http.Error(w, "City name parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	weather, err := weatherAPI(cityName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(weather)
+}
 
